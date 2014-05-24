36322257f33e3777e2ad56fe5d6dd7b85a1c1f85
Stephen Mizell <smizell@gmail.com>
2014-05-24 11:02:56 -0400

    Add fields to index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var semantics = require('./lib/semantics'),
     links = require('./lib/links'),
     actions = require('./lib/actions'),
     queries = require('./lib/queries'),
+    fields = require('./lib/fields'),
     representer = require('./representer')
 
 module.exports = {
@@ -9,5 +10,6 @@ module.exports = {
   links: links,
   actions: actions,
   queries: queries,
+  fields: fields,
   representer: representer
 }
