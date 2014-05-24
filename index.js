db26d98b5cc2600cc3bb96e86126d1bbe2b97a5f
Stephen Mizell <smizell@gmail.com>
2014-05-24 11:00:40 -0400

    Add semantics to index.js file

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-var links = require('./lib/links'),
+var semantics = require('./lib/semantics'),
+    links = require('./lib/links'),
     actions = require('./lib/actions'),
     queries = require('./lib/queries'),
     representer = require('./representer')
 
 module.exports = {
+  semantics: semantics,
   links: links,
   actions: actions,
   queries: queries,
